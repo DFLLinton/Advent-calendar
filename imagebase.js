e878d3d514c25a584e00148d458b25854e87555a
DFLLinton <dominiclinton@gmail.com>
2022-11-26 14:18:51 +0000

    Update imagebase.js

--- imagebase.js
+++ imagebase.js
@@ -1,7 +1,7 @@
 
 function findCorrectImage(y){
     if (y==1){
-      return `url('https://img.freepik.com/premium-vector/merry-christmas-lettering-quote-decorated-with-hand-drawn-reindeer_67074-1467.jpg?w=2000')`
+      return `url('https://media2.giphy.com/media/3o6ZtjqdcQfuno2WPK/giphy.gif?cid=ecf05e47gzjvt599mkvsjij8visvz5ktzewjr55cqh24hd5w&rid=giphy.gif&ct=g')`
     }if (y==2){
       return `url('https://helloartsy.com/wp-content/uploads/kids/christmas/easy-santa-claus-drawing/easy-santa-claus-drawing-step-6-1024x1024.jpg')`
     }if (y==3){
@@ -11,7 +11,7 @@ function findCorrectImage(y){
     }if (y==5){
       return `url('https://helloartsy.com/wp-content/uploads/kids/christmas/how-to-draw-a-christmas-penguin/how-to-draw-a-christmas-penguin-step-6-768x1024.jpg')`
     }if (y==6){
-      return `url('https://d3rdz6pl3b4fcj.cloudfront.net/wp-content/uploads/2015/12/hamsters-in-christmas-stockings.jpg')`
+      return `url('https://media2.giphy.com/media/D7jF5I7enYdOw/giphy.gif?cid=ecf05e47v3xmedfaeixg8uiq37qmoh6nromgl9sxosn4yakm&rid=giphy.gif&ct=g')`
     }if (y==7){
       return `url('https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=839,format=auto/uploads/project/321178/cover_1242_5990a3c80bbc846bd30b2e95f49e71ab.jpg')`
     }if (y==8){
@@ -21,11 +21,11 @@ function findCorrectImage(y){
     }if (y==10){
       return `url('https://artprojectsforkids.org/wp-content/uploads/2021/10/How-to-Draw-the-Grinch.jpg')`
     }if (y==11){
-      return `url('https://img.freepik.com/premium-vector/cute-alpaca-costume-christmas-hand-drawn-cartoon-style-vector_42349-184.jpg?w=2000')`
+      return `url('https://media1.giphy.com/media/gNke2UrUTopOg/giphy.gif?cid=ecf05e47gzjvt599mkvsjij8visvz5ktzewjr55cqh24hd5w&rid=giphy.gif&ct=g')`
     }if (y==12){
       return `url('https://c8.alamy.com/comp/2A8PF0H/cute-cartoon-french-bulldog-puppy-in-santa-claus-hat-inside-hanging-christmas-stocking-merry-christmas-greeting-card-with-dog-drawing-in-a-red-sock-2A8PF0H.jpg')`
     }if (y==13){
-      return `url('https://images.template.net/114902/cute-christmas-drawing-aqr84.jpg')`
+      return `url('https://media3.giphy.com/media/11BEQyXROgnLTG/giphy.gif?cid=ecf05e47gzjvt599mkvsjij8visvz5ktzewjr55cqh24hd5w&rid=giphy.gif&ct=g')`
     }if (y==14){
       return `url('https://www.kindpng.com/picc/m/491-4916013_sapin-01-xmas-bw-cute-christmas-tree-drawings.png')`
     }if (y==15){
@@ -35,7 +35,7 @@ function findCorrectImage(y){
     }if (y==17){
       return `url('https://c8.alamy.com/comp/2D951AH/cute-character-a-green-christmas-tree-with-a-face-a-smile-and-a-sticking-out-tongue-watercolor-hand-drawing-on-a-white-background-for-new-years-2D951AH.jpg')`
     }if (y==18){
-      return `url('https://cdnb.artstation.com/p/assets/images/images/005/382/401/large/sally-morris-026.jpg?1490638097')`
+      return `url('https://media2.giphy.com/media/wPn31WOCzkvKg/giphy.gif?cid=ecf05e47hmf5qdq0hd989k1lk22t0tz8lcz2l109o6j70ziv&rid=giphy.gif&ct=g')`
     }if (y==19){
       return `url('https://www.kindpng.com/picc/m/440-4408064_spongebob-christmas-christmasspongebob-spongebobchristmas-cute-christmas-drawings-easy.png')`
     }if (y==20){
@@ -45,7 +45,7 @@ function findCorrectImage(y){
     }if (y==22){
       return `url('https://thumbs.dreamstime.com/b/distressed-sticker-cute-christmas-frog-creative-illustrated-147680191.jpg')`
     }if (y==23){
-      return `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxTTDjgBYm38-V0iaJ-PRU_T-grwrTBHUziw&usqp=CAU')`
+      return `url('https://media4.giphy.com/media/11vPVDtI88cMHS/giphy.gif?cid=ecf05e47hmf5qdq0hd989k1lk22t0tz8lcz2l109o6j70ziv&rid=giphy.gif&ct=g')`
     }if (y==24){
       return `url('https://banner2.cleanpng.com/20180309/oww/kisspng-snowman-christmas-clip-art-creative-cute-snowman-5aa2979b0757d3.2068684315206050830301.jpg')`
     }
