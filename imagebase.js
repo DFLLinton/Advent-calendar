fd5f7e4c84f3904c1875d6a7fe107c0da216aec7
DFLLinton <dominiclinton@gmail.com>
2022-11-26 13:45:38 +0000

    Saturday update

---
+++ imagebase.js
@@ -0,0 +1,11 @@
+
+function findCorrectImage(y){
+    if (y==1){
+      return `url('https://johnlewis.scene7.com/is/image/JohnLewis/christmas-hub-button-carousel-nav-trees-3x4-141022')`
+    }else{
+      return `url('host.jpg')`
+    }
+  }
+  
+  
+  
--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function openDoor(x){
-    console.log(dd);
-    console.log(mm);
-    if (mm >= 11 && dd >= x){
-   document.getElementById("door"+x).style.opacity="0"
-    }else{};
-  }
-  
-  
-  
-  //get date
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = today.getFullYear();
-  
-  today = mm + '/' + dd + '/' + yyyy;
-  document.write(today);
+  if (mm >= 11 && dd >= x){
+document.getElementById("item"+x).style.backgroundImage=findCorrectImage(x);  
+document.getElementById("item"+x).style.color="red";
+    localStorage.setItem('item'+x, 'open');
+}else{};
+}
+
+
+
+//get date
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+
